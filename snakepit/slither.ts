7b2e667cb594fe943986dcd93476a1efebd58d6b
Sebastian Berglönn <sebastianberglonn@gmail.com>
2023-04-17 19:31:49 +0200

    minor fix

--- snakepit/slither.ts
+++ snakepit/slither.ts
@@ -5,11 +5,17 @@ import { GameSettings, Direction, RelativeDirection, GameMode } from '../src/typ
 import type { GameStartingEventMessage, Message, SnakeDeadEventMessage } from '../src/types_messages';
 const directions = [Direction.Up, Direction.Down, Direction.Left, Direction.Right];
 
+let maxTick = 250;
+
 export async function getNextMove(gameMap: GameMap, gameSettings: GameSettings, gameTick: number) {
   const potentialMoves = directions.filter(dir => gameMap.playerSnake.canMoveInDirection(dir));
-  const mapOfMoves = new Map<Direction, number>(potentialMoves.map(dir => [dir, 0]));
+  if (potentialMoves.length == 1) {
+    return potentialMoves[0]
+  }
 
+  const mapOfMoves = new Map<Direction, number>(potentialMoves.map(dir => [dir, 0]));
   potentialMoves.forEach(dir => checkAround(gameMap, dir, mapOfMoves, gameMap.playerSnake.headCoordinate.translateByDirection(dir), 0));
+
   let highestValue = { dir: Direction.Up, v: 0 }
   mapOfMoves.forEach((value, key) => {
     if (value > highestValue.v) {
@@ -20,7 +26,8 @@ export async function getNextMove(gameMap: GameMap, gameSettings: GameSettings, 
 }
 
 function checkAround(gameMap: GameMap, initialMovement: Direction, mapOfMoves: Map<Direction, number>, headCoords: Coordinate, depth: number) {
-  if (depth == 9) {
+  const maxDepth = maxTick > 500 ? 9 : 8;
+  if (depth == maxDepth) {
     return null;
   }
   const getPotentionalMoves = directions.filter(dir => {
@@ -37,7 +44,7 @@ function checkAround(gameMap: GameMap, initialMovement: Direction, mapOfMoves: M
 export function onMessage(message: Message) {
   switch (message.type) {
     case MessageType.GameStarting:
-      message = message as GameStartingEventMessage; // Cast to correct type
+      maxTick = (message as GameStartingEventMessage).gameSettings.timeInMsPerTick
       // Reset snake state here
       break;
     case MessageType.SnakeDead:
@@ -49,6 +56,7 @@ export function onMessage(message: Message) {
 
 // Settings ommitted are set to default values from the server, change this if you want to override them
 export const trainingGameSettings = {
+  // timeInMsPerTick: 3000
   // maxNoofPlayers: 2,
   // obstaclesEnabled: false,
   // ...
